df7ae0acab17148cd29973ce36a7157e0aa9a527
Niyaa1786 <j4code1786@gmail.com>
2026-05-10 20:30:14 +0700

    feat(infra): setup AppDbContext and dependency injection

---
+++ backend/SimpleAuthWithCleanArchitecture/SimpleAuth.Infrastructure/AddInfrastructureDI.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleAuth.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SimpleAuth.Infrastructure
+{
+    public static class AddInfrastructureDI
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+
+            return services;
+        }
+    }
+}
---
+++ backend/SimpleAuthWithCleanArchitecture/SimpleAuth.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleAuth.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleAuth.Infrastructure.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Username).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.PasswordHash).IsRequired();
+                entity.Property(e => e.RefreshToken).HasMaxLength(256);
+                entity.HasIndex(e => e.Username).IsUnique();
+                entity.HasIndex(e => e.Email).IsUnique();
+            });
+        }
+    }
+}
