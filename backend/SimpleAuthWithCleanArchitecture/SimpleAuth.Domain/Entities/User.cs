ab978aaf8ef70dcb1fefee322a1c657f5b21732e
Niyaa1786 <j4code1786@gmail.com>
2026-05-10 18:51:45 +0700

    feat(domain): add User entity and IUserRepository interface

---
+++ backend/SimpleAuthWithCleanArchitecture/SimpleAuth.Domain/Entities/User.cs
@@ -0,0 +1,44 @@
+﻿using SimpleAuth.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleAuth.Domain.Entities
+{ 
+    public class User
+    {
+        public Guid Id { get;private set;  }
+        public string Username { get; private set; } = string.Empty;
+        public string Email { get; private set; } = string.Empty;
+        public string PasswordHash { get; private set; } = string.Empty;
+        public UserRole Role { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        public string? RefreshToken { get; private set; }
+        public DateTime RefreshTokenExpiry { get; private set; }
+
+        private User() { }
+        
+        public User(string username, string email, string passwordHash, UserRole role = UserRole.User)
+        {
+            Id = Guid.NewGuid();
+            Username = username;
+            Email = email;
+            PasswordHash = passwordHash;
+            Role = role;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void SetRefreshToken(string refreshToken, DateTime expiry)
+        {
+            RefreshToken = refreshToken;
+            RefreshTokenExpiry = expiry;
+        }
+
+        public void RevokeRefreshToken()
+        {
+            RefreshToken = null;
+            RefreshTokenExpiry = DateTime.MinValue;
+        }
+    }
+}
---
+++ backend/SimpleAuthWithCleanArchitecture/SimpleAuth.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using SimpleAuth.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleAuth.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllAsync(CancellationToken ct = default);
+        Task<User> GetByIdAsync(Guid id, CancellationToken ct = default);
+        Task<User> GetByNameAsync(string name, CancellationToken ct = default);
+        Task<User> GetByEmailAsync(string email, CancellationToken ct = default);
+        Task<User> GetByNameOrEmailAsync(string name, string email, CancellationToken ct = default);
+        Task<User> GetByRefreshTokenAsync(string refreshToken, CancellationToken ct = default);
+
+        void  Add(User user);
+        void Update(User user);
+        void Delete(User user);
+
+    }
+}
