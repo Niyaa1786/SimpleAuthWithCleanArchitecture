62e9900f467944564c9efdc5036e5bdf4f9821cd
Niyaa1786 <j4code1786@gmail.com>
2026-05-12 17:43:06 +0700

    feat(auth): configure JWT authentication with token validation

--- backend/SimpleAuthWithCleanArchitecture/SimpleAuth.Api/Program.cs
+++ backend/SimpleAuthWithCleanArchitecture/SimpleAuth.Api/Program.cs
@@ -1,12 +1,18 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 using Serilog;
+using Serilog.Events;
+using SimpleAuth.Api.Handler;
 using SimpleAuth.Application;
 using SimpleAuth.Infrastructure;
+using System.Text;
 using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
     .WriteTo.Console()
     .CreateLogger();
 
@@ -20,7 +26,28 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+// Jwt Authentication
+var jwtSettings = builder.Configuration.GetSection("JWT");
+var serectKey = jwtSettings["SecretKey"] ?? throw new Exception("Jwt SecrectKey missing");
+var key = Encoding.UTF8.GetBytes(serectKey);
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)  
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings["Issuer"],
+                ValidAudience = jwtSettings["Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(key)
+            };
+        });
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -36,8 +63,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseExceptionHandler();
 
 app.Run();
